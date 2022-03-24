5e7e3fa2f123e18faf0880e75386a61f92576b91
klyssmannoliveira <klyssmannoliveira@gmail.com>
2022-03-23 23:07:37 -0300

    DLL da habilidade

--- docs/modulo4/DDL.sql
+++ docs/modulo4/DDL.sql
@@ -3,14 +3,53 @@ createdb starWars
 \l # listar os bancos de dados;
 \c starWars; # Entrar no data base;
 
-CREATE TABLE campo_astromico(
- ID NUMERIC(10) NOT NULL,
- Nome VARCHAR(100) NOT NULL,
- Setor VARCHAR(100) NOT NULL,
- RegiaoAstronomica VARCHAR(100) NOT NULL,
- Atmosfera VARCHAR(100) NOT NULL,
+CREATE TABLE campo_astronomico(
+ id SERIAL PRIMARY KEY,
+ nome VARCHAR(100) NOT NULL,
+ setor VARCHAR(100) NOT NULL,
+ regiaoAstronomica VARCHAR(100) NOT NULL,
+ atmosfera VARCHAR(100) NOT NULL,
 
- CONSTRAINT campo_pk PRIMARY KEY(ID),
- CONSTRAINT campo_un UNIQUE(Nome),
- CONSTRAINT campo_atmosfera_ck CHECK(Atmosfera IN ('Respirável','Não Respirável'))
+ CONSTRAINT campo_un UNIQUE(nome),
+ CONSTRAINT campo_atmosfera_ck CHECK(atmosfera IN ('Respirável','Não Respirável'))
+);
+
+CREATE TABLE habilidade(
+ id SERIAL PRIMARY KEY,
+ nome VARCHAR(100) NOT NULL,
+ descricao VARCHAR(100) NOT NULL,
+ nivel SMALLINT NOT NULL,
+ impacto INTEGER NOT NULL,
+ preRequisito VARCHAR(100) NOT NULL,
+
+ CONSTRAINT habilidade_nome_un UNIQUE(nome),
+ CONSTRAINT habilidade_nivel_ck CHECK( (nivel >= 1) AND (nivel <= 100)),
+ CONSTRAINT habilidade_impacto_ck CHECK( impacto >= 0 )
+);
+
+CREATE TABLE habilidade_arma(
+  id SERIAL PRIMARY KEY,
+  habilidade SERIAL,
+  tipo VARCHAR(100) NOT NULL,
+  municao SMALLINT NOT NULL,
+
+  CONSTRAINT habilidade_arma_municao_ck CHECK( (municao >= 0) AND (municao <= 100)),
+  CONSTRAINT habilidade_habArma_fk FOREIGN KEY (habilidade) REFERENCES habilidade (id) ON DELETE CASCADE
+);
+
+CREATE TABLE habilidade_droid(
+  id SERIAL PRIMARY KEY,
+  habilidade SERIAL,
+  aspecto VARCHAR(100) NOT NULL,
+
+  CONSTRAINT habilidade_habDroid_fk FOREIGN KEY (habilidade) REFERENCES habilidade (id) ON DELETE CASCADE
+);
+
+CREATE TABLE habilidade_forca(
+  id SERIAL PRIMARY KEY,
+  habilidade SERIAL,
+  classificacao VARCHAR(100) NOT NULL,
+  aspecto VARCHAR(100) NOT NULL,
+
+  CONSTRAINT habilidade_habForca_fk FOREIGN KEY (habilidade) REFERENCES habilidade (id) ON DELETE CASCADE
 );
