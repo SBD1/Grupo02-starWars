2a79aa1e2cd7d1cd24fb5be8122573897926f2d1
klyssmannoliveira <klyssmannoliveira@gmail.com>
2022-03-28 21:42:47 -0300

    Que a força esteja com você e o primeiro episódio em sql

--- docs/modulo4/populacao-banco/script-popula.sql
+++ docs/modulo4/populacao-banco/script-popula.sql
@@ -83,3 +83,6 @@ INSERT INTO nave(nro_serie, nome, descricao, velocidade_maxima, arma) VALUES ('M
 INSERT INTO nave(nro_serie, nome, descricao, velocidade_maxima, arma) VALUES ('AC-3640', 'A Arca', 'Cargueiro', 150, 'Não possui');
 INSERT INTO nave(nro_serie, nome, descricao, velocidade_maxima, arma) VALUES ('CR-400', 'Cruzador leve Hutt', 'Cruzador leve', 560, 'Lançadores de mísseis de concussão');
 
+-- Roteiro
+
+INSERT INTO roteiro(titulo, historia, abertura) VALUES ('Episódio I - Parte I - Surgimento dos clones', 'Um jovem padawan inicia sua trajetória treinando para se tornar Jedi e ajudar o seu povo na guerra. Sua trajetória terá muitos desafios que colocará em dúvida a sua integridade moral. Qual caminho ele seguirá?', 'Uma inquietação toma conta do Senado Galáctico. Milhares de sistemas solares declararam suas intenções em deixar a República. Esse movimento separatista, sob a liderança do misterioso Conde Dookan, tem criado dificuldades para o limitado número de Cavaleiros Jedi manter a paz e a ordem na galáxia. Senadora Amidala, a antiga rainha de Naboo, está retornando ao Senado Galáctico para a votação da proposta crítica de criação de um Exército da República, para auxiliar os sobrecarregados Jedi...');
