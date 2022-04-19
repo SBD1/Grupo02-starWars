d27431f409755b004c7d2fe3525f0ae9ccb7af4b
João Gabriel de Matos <joaogabriel.c.matos@gmail.com>
2022-04-18 23:01:36 -0300

    Altera script gerador de tabela.

--- docs/modulo4/tabelas-banco/script-tabelas.sql
+++ docs/modulo4/tabelas-banco/script-tabelas.sql
@@ -264,9 +264,12 @@ CREATE TABLE IF NOT EXISTS localizacao(
   instancia_item SERIAL,
   instancia_oponente SERIAL,
   instancia_de_nave SERIAL,
+  npc SERIAL,
 
   CONSTRAINT regiao_localizacao_fk FOREIGN KEY (regiao) REFERENCES regiao (id) ON DELETE SET NULL,
   CONSTRAINT instItem_localizacao_fk FOREIGN KEY (instancia_item) REFERENCES instancia_item (id) ON DELETE SET NULL,
   CONSTRAINT instOponente_localizacao_fk FOREIGN KEY (instancia_oponente) REFERENCES instancia_oponente (id) ON DELETE SET NULL,
   CONSTRAINT instNave_localizacao_fk FOREIGN KEY (instancia_de_nave) REFERENCES instancia_de_nave (id) ON DELETE SET NULL
+  CONSTRAINT npc_fk FOREIGN KEY (npc) REFERENCES npc (id) ON DELETE SET NULL
+
 );
