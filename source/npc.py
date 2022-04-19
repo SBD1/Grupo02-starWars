2d4cb2eb747190ba5832f2d6ef551da65a2f380a
João Gabriel de Matos <joaogabriel.c.matos@gmail.com>
2022-04-18 22:25:30 -0300

    Adiciona função de dialogo.

---
+++ source/npc.py
@@ -0,0 +1,17 @@
+from db.database import connect, query, close
+
+
+def npc_dialogo(npc_id):
+    [cursor, connection] = connect()
+
+    npc = query(cursor, f"SELECT * FROM npc WHERE id='{npc_id}';")
+
+    close(connection, cursor)
+
+    print(f'''
+          
+    {npc.nome}:
+    
+          {npc.dialogo}
+          
+          ''')
