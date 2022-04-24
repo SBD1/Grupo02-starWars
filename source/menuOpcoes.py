9e98e1498d97dceac1968a95a884c661bf473cbe
klyssmannoliveira <klyssmannoliveira@gmail.com>
2022-04-24 00:34:30 -0300

    Ajustes arquivos da consulta do consultaInventario
    Co-authored-by: Luiz Henrique <luizhemrique.fnandes@gmail.com>

--- source/menuOpcoes.py
+++ source/menuOpcoes.py
@@ -1,3 +1,4 @@
+from consultaInventario import consultaInventario
 from db.database import connect, query, close
 from interacao import consultaLocalização
 from objetivo import consultaTodosObjetivo
@@ -174,28 +175,3 @@ def consultaRegiao(latitude, longitude):
     input(f'\n\nAperte qualquer tecla para sair: ')
 
     return 0
-
-
-def consultaInventario(jogador):
-
-    [cursor, connection] = connect()
-    idjogador = query(
-        cursor, f'select id from jogador where nome = \'{jogador}\';')
-    idjogador = idjogador[0]
-
-    inventario = query(
-        cursor, f'select item from instancia_item_jogador where jogador = {idjogador[0]};')
-    if not inventario:
-        print('\nO inventário está vazio!\n\n')
-    else:
-        print('\n')
-        i = 0
-        for row in inventario:
-            item = query(
-                cursor, f'select nome from item where id = {row[0]};')
-            item = item[0]
-            i = i+1
-            print(f'{i} - {item[0]}')
-        print('\n')
-    close(connection, cursor)
-    input(f'\n\nAperte qualquer tecla para sair: ')
