7d01ebbbece47abce2979899cd3f4f078374b3f4
klyssmannoliveira <klyssmannoliveira@gmail.com>
2022-04-24 00:29:05 -0300

    Acesso ao inventário do jogador
    Co-authored-by: Luiz Henrique <luizhemrique.fnandes@gmail.com>

--- source/menuOpcoes.py
+++ source/menuOpcoes.py
@@ -68,8 +68,7 @@ def menu(player, titulo, id_campoastronomico):
             consultaRegiao(latitude, longitude)
 
         if (option == 'I' or option == 'i'):
-            print(
-                'Acessa inventário --> Faz consulta no banco dos itens que o jogador possui')
+            consultaInventario(player)
         if (option == 'C' or option == 'c'):
             consultaCaracteristica(player)
         if (option == 'R' or option == 'r'):
@@ -175,3 +174,28 @@ def consultaRegiao(latitude, longitude):
     input(f'\n\nAperte qualquer tecla para sair: ')
 
     return 0
+
+
+def consultaInventario(jogador):
+
+    [cursor, connection] = connect()
+    idjogador = query(
+        cursor, f'select id from jogador where nome = \'{jogador}\';')
+    idjogador = idjogador[0]
+
+    inventario = query(
+        cursor, f'select item from instancia_item_jogador where jogador = {idjogador[0]};')
+    if not inventario:
+        print('\nO inventário está vazio!\n\n')
+    else:
+        print('\n')
+        i = 0
+        for row in inventario:
+            item = query(
+                cursor, f'select nome from item where id = {row[0]};')
+            item = item[0]
+            i = i+1
+            print(f'{i} - {item[0]}')
+        print('\n')
+    close(connection, cursor)
+    input(f'\n\nAperte qualquer tecla para sair: ')
